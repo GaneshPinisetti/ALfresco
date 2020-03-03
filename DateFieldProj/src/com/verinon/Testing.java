cc4aff0ce51e0e6965421bf241590c0e14b925ce
GaneshPinisetti <38444359+GaneshPinisetti@users.noreply.github.com>
2020-03-04 02:29:08 +0530

    Method updated

--- DateFieldProj/src/com/verinon/Testing.java
+++ DateFieldProj/src/com/verinon/Testing.java
@@ -4,6 +4,12 @@ public class Testing {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
+		int a=5;
+		int b=6;
+		int c=a+b;
+		System.out.println("nbr is "+c);
 
 	}
 
